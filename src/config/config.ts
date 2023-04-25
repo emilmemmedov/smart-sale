1fc4c0ddd34877daebc2794381c6e8fb833a2fb6
Emil Məmmədov <emill.memmedovv@gmail.com>
2023-04-25 19:32:03 +0400

    config joi

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,16 @@
 import 'dotenv/config';
 import * as process from 'process';
+import { logger } from '../logging/logger';
+const Joi = require('joi');
+export const ConfigSchema = Joi.object({
+  PORT: Joi.required(),
+  DB_PG_HOST: Joi.string().required(),
+  DB_PG_USERNAME: Joi.string().required(),
+  DB_PG_PORT: Joi.required(),
+  DB_PG_NAME: Joi.string().required(),
+  DB_PG_PASSWORD: Joi.string().required(),
+});
+
 export const config = {
   PORT: process.env.PORT,
   DB_PG_HOST: process.env.DB_PG_HOST,
@@ -8,3 +19,10 @@ export const config = {
   DB_PG_NAME: process.env.DB_PG_NAME,
   DB_PG_PASSWORD: process.env.DB_PG_PASSWORD,
 };
+
+const validation = ConfigSchema.validate(config);
+
+if (validation['error']) {
+  logger.error(JSON.stringify(validation['error']['details']));
+  process.exit();
+}
